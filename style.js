824b4eb6e6962fcd36837d2699448574bbc19a23
Qiuji Zhao <qiujizhao@Qiujis-MacBook-Pro.local>
2018-10-30 16:18:47 -0400

    All

---
+++ style.js
@@ -0,0 +1,94 @@
+anime({
+  targets: 'div.box.red',
+  translateY: [
+    { value: -500, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+
+  //scale: 0.5,
+   borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+
+anime({
+  targets: 'div.box.blue',
+  translateY: [
+    { value: -450, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+    borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+
+anime({
+  targets: 'div.box.green',
+  translateY: [
+    { value: -400, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+     borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+
+anime({
+  targets: 'div.box.yellow',
+  translateY: [
+    { value: -350, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+     borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+anime({
+  targets: 'div.box.lan',
+  translateY: [
+    { value: -300, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+     borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+
+anime({
+  targets: 'div.box.purple',
+  translateY: [
+    { value: -250, duration: 1000 },
+    { value: 0, duration: 800 }
+  ],
+     borderRadius: ['0em', '3em'],
+  easing: 'easeInOutQuad',
+  rotate: {
+    value:'1turn',
+    easing: 'easeInOutSine'
+}
+});
+
+// anime({
+//   targets: 'div.box.blue',
+//   left: "80%",
+//   opacity:0.5,
+//   backgroundColor:'#DDD'
+//   // borderRadius: ['0em','2em']
+//   // easing:'easeInOutQuad'
+  
+// });
